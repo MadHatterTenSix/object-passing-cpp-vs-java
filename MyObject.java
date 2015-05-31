e754fd844f458e37dd1213be22e6542618328798
MadHatterTenSix <beastwood2013@gmail.com>
2015-05-31 15:11:47 -0600

    Create MyObject.java

---
+++ MyObject.java
@@ -0,0 +1,18 @@
+/* MyObject.java */
+
+public class MyObject {
+
+  private int _x;
+  private int _y;
+
+  public MyObject(int x, int y) {
+    _x = x;
+    _y = y;
+  }
+
+  public int getX() { return _x; }
+  public int getY() { return _y; }
+  public void setX(int x) { _x = x; }
+  public void setY(int y) { _y = y; }
+
+}

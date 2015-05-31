212b53f3f04d6e8d205a2716151958f427a5ebe1
MadHatterTenSix <beastwood2013@gmail.com>
2015-05-31 15:10:26 -0600

    Update MyObject.cpp

--- MyObject.cpp
+++ MyObject.cpp
@@ -2,8 +2,20 @@
 
 #include "MyObject.h"
 
-MyObject::MyObject(int x, int y) { _x = x; _y = y; }
-int MyObject::getX() { return _x; }
-int MyObject::getY() { return _y; }
-void MyObject::setX(int x) { _x = x; }
-void MyObject::setY(int y) { _y = y; }
+MyObject::MyObject(int x, int y)
+{
+  _x = x;
+  _y = y;
+}
+
+int MyObject::getX()
+  { return _x; }
+  
+int MyObject::getY()
+  { return _y; }
+  
+void MyObject::setX(int x)
+  { _x = x; }
+  
+void MyObject::setY(int y)
+  { _y = y; }
